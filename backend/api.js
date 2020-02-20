dd31740dac39634c69705abca1be9b07b6de694b
Grant Barnes <gfbarnes78@gmail.com>
2020-02-19 18:52:31 -0600

    adding authentication

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const authentication = require("./authentication.js");
 const objects = require("./objects.js");
+
+function returnSuccess(response) {
+    response.status(200).send("success");
+    response.end();
+}
+
+function rejectUnauthorized(response) {
+    authentication.removeTokenCookie(response);
+    response.status(404).send("not authorized");
+    response.end();
+}
+
+function returnResponse(response, result) {
+    response.writeHead(result.statusCode, {
+        "Content-Type": "application/json"
+    });
+    response.write(JSON.stringify(result.data));
+    response.end();
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -9,38 +29,44 @@ const objects = require("./objects.js");
 
 // Heartbeat to make sure server is running
 router.get("/api/heartbeat", (request, response) => {
-    response.status(200).send("success");
-    response.end();
+    returnSuccess(response);
+});
+
+// Get edit token if body is correct
+router.post("/api/token", (request, response) => {
+    if (authentication.requestToken(request.body)) {
+        authentication.setTokenCookie(response);
+        returnSuccess(response);
+    } else {
+        rejectUnauthorized(response);
+    }
 });
 
 // Delete object by ID
 router.delete("/api/object/:id", (request, response) => {
-    const result = objects.deleteObject(request.params.id, user);
-    response.writeHead(result.statusCode, {
-        "Content-Type": "application/json"
-    });
-    response.write(JSON.stringify(result.data));
-    response.end();
+    if (!authentication.isAuthorized(request)) {
+        rejectUnauthorized(response);
+        return;
+    }
+    returnResponse(response, objects.deleteObject(request.params.id, user));
 });
 
 // Update existing object
 router.put("/api/object", (request, response) => {
-    const result = objects.updateObject(request.body, user);
-    response.writeHead(result.statusCode, {
-        "Content-Type": "application/json"
-    });
-    response.write(JSON.stringify(result.data));
-    response.end();
+    if (!authentication.isAuthorized(request)) {
+        rejectUnauthorized(response);
+        return;
+    }
+    returnResponse(response, objects.updateObject(request.body, user));
 });
 
 // Create new object
 router.post("/api/object", (request, response) => {
-    const result = objects.createObject(request.body, user);
-    response.writeHead(result.statusCode, {
-        "Content-Type": "application/json"
-    });
-    response.write(JSON.stringify(result.data));
-    response.end();
+    if (!authentication.isAuthorized(request)) {
+        rejectUnauthorized(response);
+        return;
+    }
+    returnResponse(response, objects.createObject(request.body, user));
 });
 
 ////////////////////////////////////////////////////////////////////////////////
---
+++ backend/authentication.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+
+const cookieName = "catp_token";
+
+function requestToken(json) {
+    if (!json) return false;
+    if (!json.edit_secret) return false;
+    if (json.edit_secret !== process.env.CATP_EDIT_SECRET) return false;
+    return true;
+}
+
+function setTokenCookie(response) {
+    const token = jwt.sign("authorized", process.env.JWT_SECRET);
+    response.cookie(cookieName, token, {
+        maxAge: 86400000, // 24 hours = 86400000 ms
+        httpOnly: true
+    });
+}
+
+function removeTokenCookie(response) {
+    response.clearCookie(cookieName);
+}
+
+function isAuthorized(request) {
+    const token = request.cookies[cookieName];
+    if (!token) return false;
+    let verify = false;
+    try {
+        verify = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {}
+    if (!verify) return false;
+    return true;
+}
+
+module.exports.requestToken = requestToken;
+module.exports.setTokenCookie = setTokenCookie;
+module.exports.removeTokenCookie = removeTokenCookie;
+module.exports.isAuthorized = isAuthorized;
